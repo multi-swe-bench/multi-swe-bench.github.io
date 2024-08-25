400d8f4a289496b8d57c03b5c10166a6eb3f537c
multi-swe-bench <elidupree@proton.me>
2024-08-25 11:10:29 +0800

    Update

--- src/About.vue
+++ src/About.vue
@@ -4,14 +4,6 @@
       <h2 class="text-title">Citation</h2>
       If you found <a href="https://www.swebench.com">SWE-bench</a> or our <a href="https://multi-swe-bench.github.io">Multi-SWE-bench</a> helpful for your work, please cite as follows:
       <p class="text-content">
-        <pre id="citation"><code>@inproceedings{jimenez2024swebench,
-    title={{SWE}-bench: Can Language Models Resolve Real-world Github Issues?},
-    author={Carlos E Jimenez and John Yang and Alexander Wettig and Shunyu Yao and Kexin Pei and Ofir Press and Karthik R Narasimhan},
-    booktitle={The Twelfth International Conference on Learning Representations},
-    year={2024},
-    url={https://openreview.net/forum?id=VTF8yNQM66}
-}</code></pre>
-        <br/>
         <pre id="citation"><code>@misc{zan2024multiswebench,
   title={SWE-bench-java: A GitHub Issue Resolving Benchmark for Java}, 
   author={xxx},
@@ -26,11 +18,9 @@
         trained and evaluated on Multi-SWE-bench can produce unexpected results.
         We are not responsible for any damages caused by the use of
         Multi-SWE-bench, including but not limited to, any loss of profit, data,
-        or use of data. 
-        <br/>
-        <b>Acknowledgements:</b>This leaderboard is a modified version of 
+        or use of data. This leaderboard is a modified version based on 
         <a href="https://github.com/swe-bench/swe-bench.github.io">the SWE-bench leaderboard</a>,
-        and we express our deepest gratitude to the creators of the SWE-bench dataset.
+        and we would like to extend our special thanks for that.
       </p>
       <p class="text-content">
         Correspondence to: <a href="mailto:daoguang@iscas.ac.cn">daoguang@iscas.ac.cn</a>
