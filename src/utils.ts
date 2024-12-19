d566ee53a9d46891e87fa00207f48e652503a149
multi-swe-bench <elidupree@proton.me>
2024-12-19 22:00:50 +0800

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -68,3 +68,36 @@ export function useLeaderboard() {
 
   return { leaderboard, language, dataset, model, languageData, datasetData, datasetResults, modelData, total }
 }
+
+export function useVisualLeaderboard() {
+  const leaderboard = ref<Language[]>()
+
+  const language = ref<string>()
+  const dataset = ref<string>()
+  const model = ref<string>()
+
+  const languageData = computed(() => leaderboard.value?.find(item => item.name === language.value)?.data)
+  const datasetData = computed(() => languageData.value?.find(item => item.name === dataset.value)?.data)
+  const datasetResults = computed(() => languageData.value?.find(item => item.name === dataset.value)?.results)
+  const total = computed(() => Object.keys(languageData.value?.find(item => item.name === dataset.value)?.data || {}).length)
+  const modelData = computed(() => datasetResults.value?.find(item => item.name === model.value))
+
+  watch(leaderboard, (items) => {
+    language.value = items?.[0]?.name
+  })
+
+  watch(languageData, (items) => {
+    dataset.value = items?.[0]?.name
+  })
+
+  watch(datasetResults, (items) => {
+    model.value = items?.[0]?.name
+  })
+
+  onMounted(async () => {
+    const response = await fetch('https://multi-swe-bench.github.io/experiments/Visual_leaderboard.json')
+    leaderboard.value = await response.json()
+  })
+
+  return { leaderboard, language, dataset, model, languageData, datasetData, datasetResults, modelData, total }
+}
