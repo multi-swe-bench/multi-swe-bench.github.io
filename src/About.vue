16a6a4d41b4b65adf97bbd161e71fdac6707bf72
liusiyao.sine <liusiyao.sine@bytedance.com>
2025-03-26 16:23:14 +0800

    feat: citation

--- src/About.vue
+++ src/About.vue
@@ -4,26 +4,6 @@
       <h2 class="text-title">Citation</h2>
       If you found <a href="https://www.swebench.com">SWE-bench</a> or our <a href="https://multi-swe-bench.github.io">Multi-SWE-bench</a> helpful for your work, please cite as follows:
       <p class="text-content">
-        <pre id="citation"><code>@inproceedings{jimenez2024swebench,
-    title={SWE-bench: Can Language Models Resolve Real-world Github Issues?},
-    author={Carlos E Jimenez and John Yang and Alexander Wettig and Shunyu Yao and Kexin Pei and Ofir Press and Karthik R Narasimhan},
-    booktitle={The Twelfth International Conference on Learning Representations},
-    year={2024},
-    url={https://openreview.net/forum?id=VTF8yNQM66}
-}</code>
-</pre>
-<br/>
-<pre id="citation">
-<code>@misc{zan2024swebenchjava,
-  title={SWE-bench-java: A GitHub Issue Resolving Benchmark for Java}, 
-  author={Daoguang Zan and Zhirong Huang and Ailun Yu and Shaoxin Lin and Yifan Shi and Wei Liu and Dong Chen and Zongshuai Qi and Hao Yu and Lei Yu and Dezhi Ran and Muhan Zeng and Bo Shen and Pan Bian and Guangtai Liang and Bei Guan and Pengjie Huang and Tao Xie and Yongji Wang and Qianxiang Wang},
-  year={2024},
-  eprint={2408.14354},
-  archivePrefix={arXiv},
-  primaryClass={cs.SE},
-  url={https://arxiv.org/abs/2408.14354}, 
-}</code></pre>
-    <br/>
 <pre id="citation">
 <code>@misc{zhang2024codevissueresolvingvisual,
       title={CodeV: Issue Resolving with Visual Data}, 
@@ -51,17 +31,8 @@
         If you're interested, feel free to join our discussions on <a href="https://discord.gg/HEU3gvyXUf">Discord</a>.
       </p>
       <div class="content-wrapper" style="display: flex; flex-direction: row;">
-        <a href="http://www.is.cas.cn" title="中国科学院软件研究所">
-          <img src="../img/iscas.png" style="height: 3em;padding-top:0.5em;padding-right: 1em" />
-        </a>
-        <a href="https://www.huawei.com" title="华为">
-          <img src="../img/huawei.png" style="height: 3em;padding-top:0.5em;padding-right: 1em" />
-        </a>
-        <a href="https://www.pku.edu.cn" title="北京大学">
-          <img src="../img/pku.png" style="height: 3em;padding-top:0.5em;padding-right: 1em" />
-        </a>
-        <a href="https://ailingzhi.com" title="灵智 AI">
-          <img src="../img/lingzhi.png" style="height: 3em;padding-top:0.5em;padding-right: 1em" />
+        <a href="https://team.doubao.com/" title="Bytedance Seed">
+          <img src="../img/seed.jpg" style="height: 3em;padding-top:0.5em;padding-right: 1em" />
         </a>
       </div>
     </div>
