9aa073f017bc07a9dc20aac41d5cb93fd647632b
25626 <2562656746@qq.com>
2025-04-01 15:13:06 +0800

    update

--- src/Header.vue
+++ src/Header.vue
@@ -19,7 +19,7 @@
         />
       </div>
       <h3 style="color: #333">
-        A <span style="color: #000; font-weight: bold;">Multilingual</span> Benchmark for Issue Resolving
+        A <span style="color: white; font-weight: bold;">Multilingual</span> Benchmark for Issue Resolving
       </h3>
       <!--<h3 style="font-size: 20px; padding-top: 1.2em">ArXiv 2024</h3>-->
           <!-- <p style="text-align: center;margin-top:1em;">
