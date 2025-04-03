ff727f25dd97b4e3a8e8144ce07e783df8dacb1f
zandaoguang <57522071+zdaoguang@users.noreply.github.com>
2025-04-03 20:52:04 +0800

    update: text in Home.vue

--- src/Home.vue
+++ src/Home.vue
@@ -105,9 +105,8 @@
         </div>
 
         <p class="text-content">
-          - The <span style="color:var(--dark_accent_color);"><b>% Resolved</b></span> metric is the percentage of instances
-          (<b>500</b> for Python, <b>128</b> for Java, <b>224</b> for TypeScript, <b>356</b> for JavaScript, <b>428</b> for Go, <b>239</b> for Rust, <b>128</b> for C, <b>129</b> for C++) <i>solved</i> by the model.
-          <b>Overall</b> represents all instances within each language, while <b>Easy</b>, <b>Medium</b>, and <b>Hard</b> denote instances at different difficulty levels.
+          - <span style="color:var(--dark_accent_color);"><b>% Resolved</b></span> denotes the proportion of successfully solved instances per language (Python: <b>500</b>, Java: <b>128</b>, TypeScript: <b>224</b>, JavaScript: <b>356</b>, Go: <b>428</b>, Rust: <b>239</b>, C: <b>128</b>, C++: <b>129</b>).
+          <b>Overall</b> includes all instances for each language, while </b>Easy</b>, </b>Medium</b>, and </b>Hard</b> correspond to subsets categorized by difficulty level.
           <br>
           - <span style="color:var(--dark_accent_color);"><b>✅ Checked</b></span> indicates that we, the Multi-SWE-bench team, received access to the system and
           were able to reproduce the patch generations.
